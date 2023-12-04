9887010a90530a52e7b2cc0e75a94fc1ed2af04f
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2023-12-04 23:50:48 +0200

    calc

--- src/route/index.js
+++ src/route/index.js
@@ -6,8 +6,11 @@ const router = express.Router()
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
 
+const calc = require('./calc')
+
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/calc', calc)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
